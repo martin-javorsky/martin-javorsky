e1077b13f147322db2ebe2c5b0d29e9a04bda8ae
Martin Javorsky <martinjavorsky@sudolabs.io>
2024-09-06 12:24:10 +0200

    Fix images display in prod

--- src/constants/common/images.ts
+++ src/constants/common/images.ts
@@ -3,5 +3,5 @@ import NextConfig from "../../../next.config.mjs";
 const basePath = NextConfig.basePath;
 
 export const images = {
-  testImage: `${basePath}/assets/test_image.jpg"`,
+  testImage: `${basePath}/assets/test_image.jpg`,
 };

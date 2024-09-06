60f10bde1c34fe5acff4e91fe56342c8af515f0f
Martin Javorsky <martinjavorsky@sudolabs.io>
2024-09-06 10:55:17 +0200

    Test image

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,5 +1,12 @@
+import Image from "next/image";
+
 function Skills() {
-  return <div>This is skills page</div>;
+  return (
+    <div>
+      This is skills page
+      <Image src="/assets/test_image.jpg" width={500} height={500} alt="image" />
+    </div>
+  );
 }
 
 export default Skills;

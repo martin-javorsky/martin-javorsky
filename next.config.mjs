dd6219b06b36381e6478edbafe3837f3b9982516
Martin Javorsky <martinjavorsky@sudolabs.io>
2024-09-06 11:41:58 +0200

    revert chenges nad fix imports

--- next.config.mjs
+++ next.config.mjs
@@ -8,31 +8,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack: (config) => {
-    config.module.rules = [
-      ...config.module.rules,
-      /**
-       * Enables tree shaking of index.ts files. This is handy when the project uses index.ts as
-       * barrel files for reexporting exports from other files (for instance, within a folder).
-       *
-       * These files shouldn't contain any side-effects.
-       * https://github.com/vercel/next.js/issues/12557#issuecomment-994278512
-       */
-      {
-        test: /index.ts/i,
-        sideEffects: false,
-      },
-      {
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      },
-    ];
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    };
-    return config;
-  },
 };
 
 export default nextConfig;

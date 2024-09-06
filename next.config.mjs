d6e37e581a58b6778dee51dcaabba1f6feb72737
Martin Javorsky <martinjavorsky@sudolabs.io>
2024-09-06 11:06:56 +0200

    Check the base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  basePath: isProduction ? "/martin-javorsky" : "",
   output: "export",
   images: {
     unoptimized: true,
